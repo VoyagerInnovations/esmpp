76ee281518849b5d1900ed059810ecd5e39dc2cb
Ruel Pagayon <ruel@ruel.me>
2017-01-13 15:57:32 +0800

    Added receiver and transceiver PDU resp (ok)

--- src/esmpp_worker.erl
+++ src/esmpp_worker.erl
@@ -30,7 +30,9 @@
 %% gen_server callbacks
 %% ----------------------------------------------------------------------------
 
-%% @private
+%% ----------------------------------------------------------------------------
+%% @private submit_sm attempt when not connected
+%% ----------------------------------------------------------------------------
 init([#{host := Host, port := Port},  BindRecord]) ->
   {pdu,    Packet} = esmpp_pdu:bind(1, BindRecord),
   {socket, Socket} = get_socket(Host, Port),
@@ -40,9 +42,15 @@ init([#{host := Host, port := Port},  BindRecord]) ->
     socket    = Socket
   }}.
 
-%% @private
+%% ----------------------------------------------------------------------------
+%% @private submit_sm attempt when not connected
+%% ----------------------------------------------------------------------------
 handle_call({submit_sm, _SubmitSm, _Options}, _From, #state{connected=false} = State) ->
   {reply, {error, not_connected}, State}; 
+
+%% ----------------------------------------------------------------------------
+%% @private submit_sm packet sending
+%% ----------------------------------------------------------------------------
 handle_call({submit_sm, SubmitSm, _Options}, From, #state{socket=Socket, seq_num=SeqNum} = State) ->
   NewSeqNum = SeqNum + 1,
   {pdu, Packet} = esmpp_pdu:submit_sm(NewSeqNum, SubmitSm), 
@@ -51,6 +59,10 @@ handle_call({submit_sm, SubmitSm, _Options}, From, #state{socket=Socket, seq_num
     from    = From,
     seq_num = NewSeqNum
   }}; 
+
+%% ----------------------------------------------------------------------------
+%% @private Catch all
+%% ----------------------------------------------------------------------------
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
 
@@ -58,7 +70,20 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Message, State) ->
   {noreply, State}.
 
+%% ----------------------------------------------------------------------------
+%% @private Successfully binded as receiver
+%% ----------------------------------------------------------------------------
+handle_info({tcp, Socket, <<_Len:32, ?BIND_RECEIVER_RESP:32, ?ESME_ROK:32, _Seq:32, _Data/binary>>}, State) ->
+  {noreply, State#state{
+    connected = true,
+    binding   = ?BIND_RECEIVER,
+    status    = ?ESME_ROK,
+    socket    = Socket
+  }};
+
+%% ----------------------------------------------------------------------------
 %% @private Successfully binded as transmitter
+%% ----------------------------------------------------------------------------
 handle_info({tcp, Socket, <<_Len:32, ?BIND_TRANSMITTER_RESP:32, ?ESME_ROK:32, _Seq:32, _Data/binary>>}, State) ->
   {noreply, State#state{
     connected = true,
@@ -67,20 +92,39 @@ handle_info({tcp, Socket, <<_Len:32, ?BIND_TRANSMITTER_RESP:32, ?ESME_ROK:32, _S
     socket    = Socket
   }};
 
+%% ----------------------------------------------------------------------------
+%% @private Successfully binded as tranceiver 
+%% ----------------------------------------------------------------------------
+handle_info({tcp, Socket, <<_Len:32, ?BIND_TRANSCEIVER_RESP:32, ?ESME_ROK:32, _Seq:32, _Data/binary>>}, State) ->
+  {noreply, State#state{
+    connected = true,
+    binding   = ?BIND_TRANSCEIVER,
+    status    = ?ESME_ROK,
+    socket    = Socket
+  }};
+
+%% ----------------------------------------------------------------------------
 %% @private Successfully sent a submit_sm request
+%% ----------------------------------------------------------------------------
 handle_info({tcp, _Socket, <<_Len:32, ?SUBMIT_SM_RESP:32, ?ESME_ROK:32, _Seq:32, _Data/binary>>}, #state{from=Client} = State) ->
   gen_server:reply(Client, ok),
   {noreply, State};
 
+%% ----------------------------------------------------------------------------
 %% @private Connection closed
 %% TODO: Reconnection
+%% ----------------------------------------------------------------------------
 handle_info({tcp_closed, _Socket}, State) ->
   {noreply, State#state{
     connected = false,
     binding   = 0
   }};
+
+%% ----------------------------------------------------------------------------
+%% @private Catch all
+%% ----------------------------------------------------------------------------
 handle_info(_Info, State) ->
-  io:format("INFO: ~p", [_Info]),
+  io:format("Warning Unknown Message: ~p", [_Info]),
   {noreply, State}.
 
 %% @private
